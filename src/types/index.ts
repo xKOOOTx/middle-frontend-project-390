35c831386f40eff3fe51b968356ba749400483b0
Alex <aegusev91@gmail.com>
2026-08-24 22:50:38 +0300

    прописал типы

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,48 @@
 // файл типов
+
+export type Status = 'confirmed' | 'cancelled'
+export interface City {
+    code: string,
+    name: string,
+    country: string
+}
+export interface Flight {
+    id: string,
+    flightNumber: string,
+    airline: {
+        code: string,
+        name: string,
+    }
+    origin: City,
+    destination: City,
+    departureAt: string,
+    arrivalAt: string,
+    durationMinutes: number,
+    price: {
+        amount: number,
+        currency: string
+    },
+    seatsAvailable: number
+}
+export interface Booking {
+    code: string,
+    status: Status,
+    flight: Flight,
+    passengers: Passenger[],
+    contact: {
+        email: string,
+        phone: string
+    }
+    totalPrice: {
+        amount: number,
+        currency: string
+    }
+    createdAt: string
+}
+
+export interface Passenger {
+    firstName: string,
+    lastName: string,
+    dateOfBirth: string
+    documentNumber: string
+}
